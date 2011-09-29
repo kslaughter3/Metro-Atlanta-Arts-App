8299264874e2d38f3d85a3295edbc67ad54dd7fa
Drew Bratcher <dbratcher@gatech.edu>
2011-09-29 00:27:06 -0400

    added view

---
+++ app/NextView.h
@@ -0,0 +1,16 @@
+//
+//  NextView.h
+//  Metro-Atlanta-Arts-App
+//
+//  Created by Drew on 9/29/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface NextView : UIViewController {
+
+}
+
+@end
