58af796376398e18f43f2d4456db95084138b05a
Anthony <agendreau3@gatech.edu>
2011-10-10 12:30:54 -0400

    Added the Artist List view and refactored the nib file names

---
+++ app/Classes/ArtistListController.h
@@ -0,0 +1,16 @@
+//
+//  ArtistListController.h
+//  Metro-Atlanta-Arts-App
+//
+//  Created by Gendreau, Anthony S on 10/10/11.
+//  Copyright 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface ArtistListController : UIViewController {
+
+}
+
+@end
